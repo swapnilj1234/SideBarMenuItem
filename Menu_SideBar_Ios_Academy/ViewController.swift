cde0cc057c342ef1cabb86edc7e58676586eeb34
swapnil <swap81733@gmail.com>
2020-07-04 22:40:39 +0530

    Initial Commit

---
+++ Menu_SideBar_Ios_Academy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Menu_SideBar_Ios_Academy
+//
+//  Created by swapnil jadhav on 04/07/20.
+//  Copyright © 2020 t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
