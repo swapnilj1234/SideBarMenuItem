c13176eafc714213be1b54be03a69627136ad0ce
swapnil <swap81733@gmail.com>
2020-07-06 11:22:18 +0530

    create class for MenuController which inherit UITableViewController define method of tableviewcontroller in the class create initialiser with define menu named register cell for table view controller

--- Menu_SideBar_Ios_Academy/ViewController.swift
+++ Menu_SideBar_Ios_Academy/ViewController.swift
@@ -28,5 +28,52 @@ class ViewController: UIViewController {
     }
     
 }
+class MenuController : UITableViewController
+    
+{
+    private let menuItem : [String]
+    
+   
+    init(with menuItem : [String])
+    {
+        
+        self.menuItem = menuItem
+        super.init(nibName: nil, bundle: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //tables
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return menuItem.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:  indexPath)
+        
+        cell.textLabel?.text = menuItem[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //relay to delegate about menu item select item
+    }
+}
+
 
 
